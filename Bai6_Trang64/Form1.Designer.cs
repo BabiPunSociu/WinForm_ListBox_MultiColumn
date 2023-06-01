951d324b2d648946d42cf730da4e57ea88a6c1d0
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-02 01:49:28 +0700

    DungNguyen

---
+++ Bai6_Trang64/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Bai6_Trang64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.lstDanhSach = new System.Windows.Forms.ListBox();
+			this.btnOK = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lstDanhSach
+			// 
+			this.lstDanhSach.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+			this.lstDanhSach.FormattingEnabled = true;
+			this.lstDanhSach.ItemHeight = 16;
+			this.lstDanhSach.Location = new System.Drawing.Point(51, 12);
+			this.lstDanhSach.Name = "lstDanhSach";
+			this.lstDanhSach.Size = new System.Drawing.Size(696, 356);
+			this.lstDanhSach.TabIndex = 0;
+			// 
+			// btnOK
+			// 
+			this.btnOK.Location = new System.Drawing.Point(51, 400);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(696, 28);
+			this.btnOK.TabIndex = 1;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.PaleGreen;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.lstDanhSach);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ListBox_Bai6Trang64";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDanhSach;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ Bai6_Trang64/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai6_Trang64
+{
+    public partial class Form1 : Form
+    {
+        string str;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Cho phép hiển thị trên nhiều cột
+            lstDanhSach.MultiColumn = true;
+            //Hiển thị trên 5 cột 
+            lstDanhSach.ColumnWidth = lstDanhSach.Width / 5;
+            //Cho phép chọn đồng thời nhiều phần tử
+            lstDanhSach.SelectionMode = SelectionMode.MultiSimple;
+            //Add dữ liệu vào hộp danh sách
+            for (int i = 1; i <= 100; i++)
+                lstDanhSach.Items.Add("Item " + i.ToString());
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            str = string.Empty;
+            if(lstDanhSach.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Bạn chưa chọn phần tử nào");
+                return;
+            }    
+
+            foreach (string item in lstDanhSach.SelectedItems)
+            {
+                str = str + item + ", ";
+            }
+            //Xóa dấu phẩy và dấu cách thừa ở cuối chuỗi
+            str = str.Remove(str.Length - 2, 2);
+            MessageBox.Show("Bạn đã chọn các phần tử : " + str,
+            "Thông báo", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+        }
+    }
+}
